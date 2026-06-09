4501d071ffb4d30a234f0e9156d1fae26a8f5e10
jasper <well.caffeinated@gmail.com>
2026-06-09 14:03:53 +0000

    tiny visual tweaks

--- src/lib/scenes/manySpheres/manySpheresDisplay.frag.glsl
+++ src/lib/scenes/manySpheres/manySpheresDisplay.frag.glsl
@@ -9,6 +9,6 @@ void main() {
   vec2 uv = vNDC * 0.5 + 0.5;
   vec3 c = texture(uTex, uv).rgb;
   // gentle exponential tonemap: 0 stays black, asymptotes to 1, tames GI fireflies
-  c = vec3(1.0) - exp(-c);
+  // c = vec3(1.0) - exp(-c);
   fragColor = vec4(c, 1.0);
 }
