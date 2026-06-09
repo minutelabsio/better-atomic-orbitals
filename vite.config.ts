19dc75e11673dc6f3ff93b4303709749851ae7ad
jasper <well.caffeinated@gmail.com>
2026-06-09 17:53:00 +0000

    more build fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import glslify from 'vite-plugin-glslify'
 import { defineConfig } from 'vite'
+import pkg from './package.json'
 
 export default defineConfig({
   plugins: [svelte(), glslify()],
+  base: process.env.NODE_ENV === 'production' ? `/${pkg.name}` : '',
 })
